c3696057aa6ff2a0009d7ea8176e7088c2152ba3
Emily Zhang <zhang11liwen@gmail.com>
2020-01-23 01:47:20 -0800

    Added Resources page and populated page with resource components.

--- otapp/ScreenContainer.js
+++ otapp/ScreenContainer.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
-import { createStackNavigator } from 'react-navigation-stack';
-import { createAppContainer } from 'react-navigation';
+import React, { Component } from "react";
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
 
 import HomeScreen from "./screens/HomeScreen";
 import ModulesPage from "./screens/ModulesPage";
+import ResourcesMap from "./screens/ResourcesMap";
 
 const NavigationStack = createStackNavigator({
-    Home: {screen: HomeScreen},
-    Modules: {screen: ModulesPage},
+  Home: { screen: HomeScreen },
+  Modules: { screen: ModulesPage },
+  Resources: { screen: ResourcesMap }
 });
 
 const Container = createAppContainer(NavigationStack);
 
-export default Container; 
+export default Container;
