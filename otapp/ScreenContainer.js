9e89e312534a35ffa966f9a0f6b621f5935791d9
Story DeWeese <sdeweese@scu.edu>
2020-02-10 21:15:26 -0800

    Add naming conventions from ewh team && add image with text

--- otapp/ScreenContainer.js
+++ otapp/ScreenContainer.js
@@ -4,14 +4,14 @@ import { createAppContainer } from "react-navigation";
 
 import HomeScreen from "./screens/HomeScreen";
 import ModulesPage from "./screens/ModulesPage";
-import Lactation from "./screens/Lactation";
+import FeedingYourBaby from "./screens/FeedingYourBaby";
 import Module1b from "./screens/Module1b";
 import ResourcesMap from "./screens/ResourcesMap";
 
 const NavigationStack = createStackNavigator({
   Home: { screen: HomeScreen },
   Modules: { screen: ModulesPage },
-  Lactation: { screen: Lactation },
+  FeedingYourBaby: { screen: FeedingYourBaby },
   Module1b: { screen: Module1b },
   Resources: { screen: ResourcesMap }
 });
