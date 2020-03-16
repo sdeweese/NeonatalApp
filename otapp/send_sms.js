985cc82e44df880f7375ac2c5f4da92ec88a51cf
Tiffany <tiffanylee97@gmail.com>
2020-03-16 13:19:52 -0700

    Add initial Twilio functionality

---
+++ otapp/send_sms.js
@@ -0,0 +1,14 @@
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Your Account Sid and Auth Token from twilio.com/console
+// DANGER! This is insecure. See http://twil.io/secure
+const accountSid = 'AC89ef0f4a10979ea72755973c2fe8beeb';
+const authToken = 'b9230a99cd59eb493898c23632a10fd8';
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+  .create({
+     body: 'OmwanaThrive gang',
+     from: '+12052362598',
+     to: '+16508636318'
+   })
+  .then(message => console.log(message.sid));
