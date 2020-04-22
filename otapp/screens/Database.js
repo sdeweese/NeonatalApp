f5001e7e1de45c136571f5282eed28b4adfea94a
Emily Zhang <emily@Emilys-MacBook-Pro.local>
2020-04-22 02:19:26 -0700

    Added save() and get() and ExpandingItem for messages portal.

--- otapp/screens/Database.js
+++ otapp/screens/Database.js
@@ -10,146 +10,178 @@ import {
   TouchableOpacity,
   View,
   AsyncStorage,
-  Button
+  Button,
 } from "react-native";
 import ExpandableItem from "./components/ExpandableItem";
 
 class Database extends React.Component {
-
-    constructor (props) {
-        super(props)
-        this.state = {
-            MotherName: '',
-            ChildName: '',
-            DoB: '',
-            Born: '',
-            Phone: '',
-            Notes: ''
-        }
-     }
-
-    setNewMother = async (key, value) => {
-        try {
-            AsyncStorage.setItem(key, JSON.stringify(value));
-            console.log("Key: " + key + " Saved Successfully");
-        } catch (error) {
-            console.log("Mother not saved properly");
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      MotherName: "",
+      ChildName: "",
+      DoB: "",
+      Born: "",
+      Phone: "",
+      Notes: "",
     };
-    
-    getNewMother = async (key) => {
-        //alert(JSON.stringify(id));
-        try {
-          let value = await AsyncStorage.getItem(key);
-          if (value !== null) {
-            console.log(value + " Returned Successfully");
-            //alert(JSON.stringify(value)); // shows all data for that user
-            //let parsed = JSON.parse(id);
-            //alert(parsed.MotherName);
-            return value;
-          } else {
-            console.log("Sorry, key not found");
-          }
-        } catch (error) {
-          console.log(error + " id not found");
-        }
-    };
+  }
 
-    removeMother = async (key) => {
-        try {
-            await AsyncStorage.removeItem(key);
-        } catch (error) {
-            console.log(error + ": error removing data");
-        }
-    };
+  setNewMother = async (key, value) => {
+    try {
+      AsyncStorage.setItem(key, JSON.stringify(value));
+      console.log("Key: " + key + " Saved Successfully");
+    } catch (error) {
+      console.log("Mother not saved properly");
+    }
+  };
 
-    saveNewMother = () => {
-        this.setNewMother("41 902 2938", {
-            MotherName: 'Mamma Name',
-            ChildName: 'Dembe',
-            DoB: 'February 29th, 2020',
-            Born: 'Yes',
-            Phone: '41 902 2938',
-            Notes: 'Tukesiga is planning to come back on April 20th'
-        })
+  getNewMother = async (key) => {
+    //alert(JSON.stringify(id));
+    try {
+      let value = await AsyncStorage.getItem(key);
+      if (value !== null) {
+        console.log(value + " Returned Successfully");
+        //alert(JSON.stringify(value)); // shows all data for that user
+        //let parsed = JSON.parse(id);
+        //alert(parsed.MotherName);
+        return value;
+      } else {
+        console.log("Sorry, key not found");
+      }
+    } catch (error) {
+      console.log(error + " id not found");
     }
-    
-    readNewMother = () => {
-        this.getNewMother("41 902 2938").then( result => {
-            let parsed = JSON.parse(result);
-            alert("Mother Name: " + parsed.MotherName + "\n" +
-                "Child's Name: " + parsed.ChildName + "\n" +
-                "Child's Birthdate: " + parsed.DoB + "\n" +
-                "Status Born:" + parsed.Born + "\n" + 
-                "Phone Number: " + parsed.Phone + "\n" +
-                "Notes: " + parsed.Notes
-                )
-        })
-    }
+  };
 
-    deleteMother = () => {
-        this.removeMother("41 902 2938")
+  removeMother = async (key) => {
+    try {
+      await AsyncStorage.removeItem(key);
+    } catch (error) {
+      console.log(error + ": error removing data");
     }
+  };
+
+  saveNewMother = () => {
+    this.setNewMother("41 902 2938", {
+      MotherName: "Mamma Name",
+      ChildName: "Dembe",
+      DoB: "February 29th, 2020",
+      Born: "Yes",
+      Phone: "41 902 2938",
+      Notes: "Tukesiga is planning to come back on April 20th",
+    });
+  };
+
+  readNewMother = () => {
+    this.getNewMother("41 902 2938").then((result) => {
+      let parsed = JSON.parse(result);
+      alert(
+        "Mother Name: " +
+          parsed.MotherName +
+          "\n" +
+          "Child's Name: " +
+          parsed.ChildName +
+          "\n" +
+          "Child's Birthdate: " +
+          parsed.DoB +
+          "\n" +
+          "Status Born:" +
+          parsed.Born +
+          "\n" +
+          "Phone Number: " +
+          parsed.Phone +
+          "\n" +
+          "Notes: " +
+          parsed.Notes
+      );
+    });
+  };
+
+  deleteMother = () => {
+    this.removeMother("41 902 2938");
+  };
 
   render() {
-    const { input } = this.state
+    const { input } = this.state;
     return (
-        
       <ScrollView style={styles.container}>
         <View style={styles.top}>
           <Text style={styles.title}>Mothers</Text>
         </View>
         <ExpandableItem title="Add a New Mother">
-            <Text>Mother's Name:</Text>
-            <TextInput style={styles.input}
-                onChangeText = {MotherName => this.setState({MotherName: MotherName})} />
+          <Text>Mother's Name:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(MotherName) =>
+              this.setState({ MotherName: MotherName })
+            }
+          />
 
-            <Text>Child's Name:</Text>
-            <TextInput style={styles.input}
-                onChangeText = {ChildName => this.setState({ChildName: ChildName})} />                   
+          <Text>Child's Name:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(ChildName) =>
+              this.setState({ ChildName: ChildName })
+            }
+          />
 
-            <Text>Date of Birth:</Text>
-            <TextInput style={styles.input}
-                onChangeText = {DoB => this.setState({DoB: DoB})} />                   
+          <Text>Date of Birth:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(DoB) => this.setState({ DoB: DoB })}
+          />
 
-            <Text>Child is Born:</Text>
-            <TextInput style={styles.input}
-                onChangeText = {Born => this.setState({Born: Born})} />                   
+          <Text>Child is Born:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(Born) => this.setState({ Born: Born })}
+          />
 
-            <Text>Phone Number:</Text>
-            <TextInput style={styles.input}
-                onChangeText = {Phone => this.setState({Phone: Phone})} />                   
+          <Text>Phone Number:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(Phone) => this.setState({ Phone: Phone })}
+          />
 
-            <Text>Notes:</Text>
-            <TextInput style={styles.input}
-                onChangeText = {Notes => this.setState({Notes: Notes})} />                   
-            <Text>{"\n"}</Text>
-            <Button style={styles.btn}
-                title="Submit"
-                onPress={() => {this.setNewMother(this.state.Phone, 
-                    { MotherName: this.state.MotherName, ChildName: this.state.ChildName, DoB: this.state.DoB, 
-                        Born: this.state.Born, Phone: this.state.Phone, Notes: this.state.Notes
-                    }
-                    )}}
-            />            
+          <Text>Notes:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(Notes) => this.setState({ Notes: Notes })}
+          />
+          <Text>{"\n"}</Text>
+          <Button
+            style={styles.btn}
+            title="Submit"
+            onPress={() => {
+              this.setNewMother(this.state.Phone, {
+                MotherName: this.state.MotherName,
+                ChildName: this.state.ChildName,
+                DoB: this.state.DoB,
+                Born: this.state.Born,
+                Phone: this.state.Phone,
+                Notes: this.state.Notes,
+              });
+            }}
+          />
           {/* {AsyncStorage.getItem(MotherName)}
           {this.props.navigation.state.params.MotherName} */}
         </ExpandableItem>
-        <ExpandableItem title="Natukunda">  
+        <ExpandableItem title="Natukunda">
           <Text style={styles.expand}>
-            Child's Name: Balondemu {'\n'}
-            Date of Birth: March 24th, 2020 {'\n'}
-            Born: Yes {'\n'}
-            Phone Number: 41 589 4931 {'\n'}
-            Notes: 
+            Child's Name: Balondemu {"\n"}
+            Date of Birth: March 24th, 2020 {"\n"}
+            Born: Yes {"\n"}
+            Phone Number: 41 589 4931 {"\n"}
+            Notes:
           </Text>
         </ExpandableItem>
         <ExpandableItem title="Tukesiga">
           <Text style={styles.expand}>
-            Child's Name: Dembe {'\n'}
-            Date of Birth: February 29th, 2020 {'\n'}
-            Born: Yes {'\n'}
-            Phone Number: 41 902 2938 {'\n'}
+            Child's Name: Dembe {"\n"}
+            Date of Birth: February 29th, 2020 {"\n"}
+            Born: Yes {"\n"}
+            Phone Number: 41 902 2938 {"\n"}
             Notes: Tukesiga is planning to come back on March 20th
           </Text>
         </ExpandableItem>
@@ -175,29 +207,29 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "space-evenly",
     color: "black",
-    flexDirection: "row"
+    flexDirection: "row",
   },
   container: {
     flex: 1,
     borderRadius: 4,
     borderWidth: 0.5,
-    borderColor: "#d6d7da"
+    borderColor: "#d6d7da",
   },
   title: {
     textAlign: "center",
     fontSize: 32,
     paddingTop: 15,
-    paddingBottom: 10
+    paddingBottom: 10,
   },
   expand: {
-    margin: 10
+    margin: 10,
   },
   input: {
     borderWidth: 1,
-    backgroundColor: 'white',
-    justifyContent: 'flex-start',
+    backgroundColor: "white",
+    justifyContent: "flex-start",
     width: 500,
     padding: 5,
-    borderRadius: 4
-  }
+    borderRadius: 4,
+  },
 });
--- otapp/screens/Messages.js
+++ otapp/screens/Messages.js
@@ -7,21 +7,86 @@ import {
   Dimensions,
   Linking,
   View,
-  Button
+  Button,
+  TextInput,
+  AsyncStorage,
 } from "react-native";
 import ExpandableItem from "./components/ExpandableItem";
 
 class Messages extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Title: "",
+      Message: "",
+      Schedule: "",
+    };
+  }
+  async saveMessage(key, value) {
+    try {
+      await AsyncStorage.setItem(key, JSON.stringify(value));
+      console.log("Key: " + key + " Saved Successfully");
+      console.log(value);
+    } catch (error) {
+      console.log(error, "Not saved properly");
+    }
+  }
+  async getMessage(key) {
+    try {
+      let value = await AsyncStorage.getItem(key);
+      if (value !== null) {
+        console.log(value + " Returned Successfully");
+        return value;
+      } else {
+        console.log("Sorry, key not found");
+      }
+    } catch (error) {
+      console.log(error, "Not found");
+    }
+  }
+  async removeMessage(key) {
+    try {
+      await AsyncStorage.removeItem(key);
+    } catch (error) {
+      console.log(error + ": error removing data");
+    }
+  }
   render() {
     return (
       <ScrollView style={styles.container}>
         <View style={styles.top}>
-          <Text style={styles.title}>Messages</Text>
-          <Button
-            title="+"
-            onPress={() => this.props.navigation.navigate("Messages")}
-          ></Button>
+          <Text style={styles.title}>Messaging Portal</Text>
         </View>
+        <ExpandableItem title="NEW MESSAGE">
+          <Text>Title:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(text) => this.setState({ Title: text })}
+          />
+
+          <Text>Message:</Text>
+          <TextInput
+            style={styles.message}
+            multiline={true}
+            onChangeText={(text) => this.setState({ Message: text })}
+          />
+
+          <Text>Scheduled for:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(text) => this.setState({ Schedule: text })}
+          />
+
+          <Button
+            title="Submit"
+            onPress={() =>
+              this.saveMessage(this.state.Title, {
+                Message: this.state.Message,
+                Schedule: this.state.Schedule,
+              })
+            }
+          />
+        </ExpandableItem>
         <ExpandableItem title="2 Days">
           <Text style={styles.expand}>
             Remember to vaccinate your child against Polio and TB right after
@@ -45,21 +110,32 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "space-evenly",
-    flexDirection: "row"
+    flexDirection: "row",
   },
   container: {
     flex: 1,
     borderRadius: 4,
     borderWidth: 0.5,
-    borderColor: "#d6d7da"
+    borderColor: "#d6d7da",
   },
   title: {
     textAlign: "center",
     fontSize: 32,
     paddingTop: 15,
-    paddingBottom: 10
+    paddingBottom: 10,
   },
   expand: {
-    margin: 10
-  }
+    margin: 10,
+  },
+  input: {
+    backgroundColor: "#f0f0f0",
+    padding: 10,
+    marginBottom: 15,
+  },
+  message: {
+    height: 300,
+    backgroundColor: "#f0f0f0",
+    padding: 10,
+    marginBottom: 15,
+  },
 });
