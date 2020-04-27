6988a070dab0c2dd1f00531fbe99db148ce86b85
Emily Zhang <emily@Emilys-MacBook-Pro.local>
2020-04-27 04:01:31 -0700

    Half figured out how to submit and display message contents!!!

---
+++ otapp/screens/components/MessagesList.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default MessagesList = ({ data }) => {
+  const messages = data; //later figure out how to sort here
+
+  return (
+    <View>
+      <Text style={styles.text}>Messages</Text>
+      {messages.map((message) => (
+        <View style={styles.text}>
+          <Text>Title: {message.title}</Text>
+          <Text>Message: {message.body}</Text>
+          <Text>Scheduled for: {message.schedule}</Text>
+        </View>
+      ))}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    textAlign: "center",
+    alignItems: "center",
+  },
+});
