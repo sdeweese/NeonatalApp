1e71539b29150db31cb767fb34985e76eb24fce2
Story DeWeese <sdeweese@scu.edu>
2020-05-04 18:28:21 -0700

    Model database after messages

--- otapp/screens/Database.js
+++ otapp/screens/Database.js
@@ -2,18 +2,19 @@ import React, { Component } from "react";
 import {
   StyleSheet,
   Text,
-  TextInput,
   ScrollView,
   Image,
   Dimensions,
   Linking,
-  TouchableOpacity,
   View,
-  AsyncStorage,
   Button,
+  TextInput,
+  AsyncStorage,
 } from "react-native";
 import ExpandableItem from "./components/ExpandableItem";
 import MotherList from "./components/MotherList";
+
+const STORAGE_KEY = "MOTHERS";
 
 class Database extends React.Component {
   constructor(props) {
@@ -25,221 +26,116 @@ class Database extends React.Component {
       Born: "",
       Phone: "",
       Notes: "",
+      mothers: [], // list of ALL the mothers
     };
   }
-
-  setNewMother = async (key, value) => {
+  componentDidMount() {
+    let m = this.getMothers();
+    this.setState({ m });
+  }
+  async saveMothers(mothers) {
     try {
-      AsyncStorage.setItem(key, JSON.stringify(value));
-      console.log("Key: " + key + " Saved Successfully");
+      await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(mothers));
     } catch (error) {
-      console.log("Mother not saved properly");
+      console.log(error, "Not saved properly");
     }
-    
-    readNewMother = () => {
-        this.getNewMother("41 902 2938").then( result => {
-            let parsed = JSON.parse(result);
-            alert("Mother Name: " + parsed.MotherName + "\n" +
-                "Child's Name: " + parsed.ChildName + "\n" +
-                "Child's Birthdate: " + parsed.DoB + "\n" +
-                "Status Born: " + parsed.Born + "\n" + 
-                "Phone Number: " + parsed.Phone + "\n" +
-                "Notes: " + parsed.Notes
-                )
-        })
-    }
-  };
+  }
 
-  removeMother = async (key) => {
+  async getMothers() {
     try {
-      await AsyncStorage.removeItem(key);
+      let mothers = await AsyncStorage.getItem(STORAGE_KEY);
+      if (mothers !== null) {
+        return JSON.parse(mothers);
+      } else {
+        return [];
+      }
+    } catch (error) {
+      console.log(error, "Not found");
+    }
+  }
+
+  async updateMother(newMother) {
+    try {
+      let mothers = await this.getMothers();
+      console.log(newMother);
+      mothers.push(newMother);
+      this.saveMothers(newMother);
+      this.setState({ newMother });
+    } catch (error) {
+      console.log("Error fetching mothers", error);
+    }
+  }
+
+  async removeMother() {
+    try {
+      await AsyncStorage.removeItem(STORAGE_KEY);
     } catch (error) {
       console.log(error + ": error removing data");
     }
-  };
-
-  saveNewMother = () => {
-    this.setNewMother("41 902 2938", {
-      MotherName: "Mamma Name",
-      ChildName: "Dembe",
-      DoB: "February 29th, 2020",
-      Born: "Yes",
-      Phone: "41 902 2938",
-      Notes: "Tukesiga is planning to come back on April 20th",
-    });
-  };
-
-  readNewMother = () => {
-    this.getNewMother("41 902 2938").then((result) => {
-      let parsed = JSON.parse(result);
-      alert(
-        "Mother Name: " +
-          parsed.MotherName +
-          "\n" +
-          "Child's Name: " +
-          parsed.ChildName +
-          "\n" +
-          "Child's Birthdate: " +
-          parsed.DoB +
-          "\n" +
-          "Status Born:" +
-          parsed.Born +
-          "\n" +
-          "Phone Number: " +
-          parsed.Phone +
-          "\n" +
-          "Notes: " +
-          parsed.Notes
-      );
-    });
-  };
-
-  deleteMother = () => {
-    this.removeMother("41 902 2938");
-  };
-
-  
-    /* renderMothers = async () => {
-      const keys = await AsyncStorage.getAllKeys();
-      const result = await AsyncStorage.multiGet(keys);
-      try {
-      
-        //console.log(result); // array of arrays 
-        //console.log(JSON.parse(result[0][1])); // all the info about the first mother in the array of arrays
-        //console.log(JSON.parse(result[0][1]).MotherName); // the name of the first mother in the array of arrays
-        
-        //alert(JSON.parse(result[0][1]).MotherName); // returns first mother in database
-
-        return ( result.map(mom => { // mom is every array (within the larger array)
-            <Text>{JSON.stringify(JSON.parse(mom[1]).ChildName)}</Text>
-            //alert(mom[0]);
-            //alert(child);
-            //return child;
-        }
-        )
-        )
-        
- 
-        
-        // sort array by mother names
-        //var sortedmoms = _.sortBy( moms, 'MotherName' ); 
-        //console.log('++++++++++++++++++++++++++++++++++++++++++++ SORTED?????????? \n' + sortedmoms);
-        
-
-        // create an expandable item for each mom -- currently the code below does not display on the page, as intended
-    
- 
-          
- 
-      } catch(error) {
-          console.log(error);
-      }
-    }
-
-  */
+  }
 
   render() {
     return (
       <ScrollView style={styles.container}>
         <View style={styles.top}>
-          <Text style={styles.title}>Mothers</Text>
+          <Text style={styles.title}>Database Portal</Text>
         </View>
-        <ExpandableItem title="Add a New Mother">
-            <Text>Mother's Name:</Text>
-            <TextInput style={styles.input}
-                onChangeText = {MotherName => this.setState({MotherName: MotherName})} 
-                value={this.state.MotherName} />
+        <View>
+          <Text>NEW MOTHER</Text>
+          <Text>Mother Name:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(text) => this.setState({ MotherName: text })}
+          />
 
-            <Text>Child's Name:</Text>
-            <TextInput style={styles.input}
-                onChangeText = {ChildName => this.setState({ChildName: ChildName})} 
-                value={this.state.ChildName}/>                   
+          <Text>Child's Name:</Text>
+          <TextInput
+            style={styles.input}
+            multiline={true}
+            onChangeText={(text) => this.setState({ ChildName: text })}
+          />
 
-            <Text>Date of Birth:</Text>
-            <TextInput style={styles.input}
-                onChangeText = {DoB => this.setState({DoB: DoB})} 
-                value={this.state.DoB}/>                   
+          <Text>Child's Birthdate:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(text) => this.setState({ DoB: text })}
+          />
 
-            <Text>Child is Born:</Text>
-            <TextInput style={styles.input}
-                onChangeText = {Born => this.setState({Born: Born})} 
-                value={this.state.Born}/>                   
+          <Text>Status Born:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(text) => this.setState({ Born: text })}
+          />
 
-            <Text>Phone Number:</Text>
-            <TextInput style={styles.input}
-                onChangeText = {Phone => this.setState({Phone: Phone})} 
-                value={this.state.Phone}/>                   
-
-            <Text>Notes:</Text>
-            <TextInput style={styles.input}
-                onChangeText = {Notes => this.setState({Notes: Notes})} 
-                value={this.state.Notes}/>                   
-            <Text>{"\n"}</Text>
-            <Button style={styles.btn}
-                title="Submit"
-                onPress={() => {
-                  if(this.state.MotherName != '' && this.state.ChildName != '' && this.state.DoB != '' && this.state.Born != '' && this.state.Phone != '' && this.state.Notes != ''){
-                  this.setNewMother(this.state.Phone, 
-                    { MotherName: this.state.MotherName, ChildName: this.state.ChildName, DoB: this.state.DoB, 
-                        Born: this.state.Born, Phone: this.state.Phone, Notes: this.state.Notes
-                    }
-                    );
-                    this.setState({
-                      Phone: '', 
-                      MotherName: '', 
-                      ChildName: '', 
-                      DoB: '', 
-                      Born: '', 
-                      Phone: '', 
-                      Notes: ''
-                      });
-                    alert(this.state.MotherName + ' saved!');
-                } else {
-                  alert('Please fill out all fields. ' + this.state.MotherName + ' was not saved.');
-                }
-              }}
-            />            
+          <Text>Number:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(text) => this.setState({ Number: text })}
+          />
           
-        </ExpandableItem>
-      
-        <ExpandableItem title="Natukunda">  
-          <Text style={styles.expand}>
-            Child's Name: Balondemu {"\n"}
-            Date of Birth: March 24th, 2020 {"\n"}
-            Born: Yes {"\n"}
-            Phone Number: 41 589 4931 {"\n"}
-            Notes:
-          </Text>
-        </ExpandableItem>
-        <ExpandableItem title="Tukesiga">
-          <Text style={styles.expand}>
-            Child's Name: Dembe {"\n"}
-            Date of Birth: February 29th, 2020 {"\n"}
-            Born: Yes {"\n"}
-            Phone Number: 41 902 2938 {"\n"}
-            Notes: Tukesiga is planning to come back on March 20th
-          </Text>
-        </ExpandableItem>
-        
-        {/* <Text>{this.renderMothers()}</Text> 
-      
-        <TouchableOpacity onPress={this.renderMothers}>
-          <Text style={styles.expand}>Render Mothers</Text>
-        </TouchableOpacity>
-*/} 
+          <Text>Notes:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(text) => this.setState({ Notes: text })}
+          />
 
-        <MotherList/> 
+          <Button
+            title="Submit"
+            onPress={() => {
+              this.updateMothers({
+                MotherName: this.state.MotherName,
+                ChildName: this.state.ChildName,
+                DoB: this.state.DoB,
+                Born: this.state.Born,
+                Phone: this.state.Phone,
+                Notes: this.state.Notes,
+              });
+            }}
+          />
+        </View>
 
+        <MotherList data={this.state.mothers} />
 
-        <TouchableOpacity onPress={this.saveNewMother}>
-          <Text style={styles.expand}>Set</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={this.readNewMother}>
-          <Text style={styles.expand}>Read</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={this.deleteMother}>
-          <Text style={styles.expand}>Remove</Text>
-        </TouchableOpacity>
       </ScrollView>
     );
   }
@@ -252,7 +148,6 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "space-evenly",
-    color: "black",
     flexDirection: "row",
   },
   container: {
@@ -271,11 +166,14 @@ const styles = StyleSheet.create({
     margin: 10,
   },
   input: {
-    borderWidth: 1,
-    backgroundColor: "white",
-    justifyContent: "flex-start",
-    width: 500,
-    padding: 5,
-    borderRadius: 4,
+    backgroundColor: "#f0f0f0",
+    padding: 10,
+    marginBottom: 15,
+  },
+  mother: {
+    height: 300,
+    backgroundColor: "#f0f0f0",
+    padding: 10,
+    marginBottom: 15,
   },
 });
--- otapp/screens/components/MotherList.js
+++ otapp/screens/components/MotherList.js
@@ -1,25 +1,23 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, Button, AsyncStorage } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 import ExpandableItem from "./ExpandableItem";
 
-export default MotherList = async () => {
-    const keys = await AsyncStorage.getAllKeys();
-    const result = await AsyncStorage.multiGet(keys);
+export default MotherList = async ({ data }) => {
+  const mothers = data;
 
   return (
     // eventually sort the names before displaying them
     <View>
-        <Text style={styles.text}>List of Mothers</Text>
-        {result.map((mom) => (
-
-            <ExpandableItem title={mom.MotherName}>
-            <Text>Mother Name: {mom.MotherName}</Text>
-            <Text>Child's Name: {mom.ChildName}</Text>
-            <Text>Child's Birthdate: {mom.DoB}</Text>
-            <Text>Status Born: {mom.Born}</Text>
-            <Text>Phone Number: {mom.Phone}</Text>
-            <Text>Notes: {mom.Notes}</Text>
-            </ExpandableItem>
+        <Text style={styles.text}>Mothers</Text>
+        {mothers.map((mom) => (
+            <View style={styles.text}>
+                    <Text>Mother Name: {mom.MotherName}</Text>
+                    <Text>Child's Name: {mom.ChildName}</Text>
+                    <Text>Child's Birthdate: {mom.DoB}</Text>
+                    <Text>Status Born: {mom.Born}</Text>
+                    <Text>Phone Number: {mom.Phone}</Text>
+                    <Text>Notes: {mom.Notes}</Text>
+            </View>
 
        ))}
     </View>
