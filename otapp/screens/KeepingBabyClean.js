604398ad3788944bf968016c30d89a86fb033740
Story DeWeese <sdeweese@scu.edu>
2020-05-11 14:10:03 -0700

    Update Hygiene module with completed slides

--- otapp/screens/KeepingBabyClean.js
+++ otapp/screens/KeepingBabyClean.js
@@ -29,6 +29,13 @@ class KeepingBabyClean extends React.Component{
                         source={require('../assets/Hygiene3.png')} />
                 </View>
 
+                <View style={styles.container}>
+                    <Image 
+                        style={{ width: Dimensions.get('window').width, height: Dimensions.get('window').height-75 }}
+                        resizeMode="contain"
+                        source={require('../assets/Hygiene4.png')} />
+                </View>
+
             </ScrollView>
         );
     }
